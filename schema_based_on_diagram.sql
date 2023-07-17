ee5fb5abd4665fb0c95477ca3b4f0436f839c2fa
Romans S <romans.spilaks@gmail.com>
2023-07-18 01:10:55 +0300

    Fix typos, change PRIMARY KEY in invoices to UNIQUE.

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -12,13 +12,19 @@ CREATE TABLE medical_histories (
   FOREIGN KEY(patient_id) REFERENCES patients(id)
 );
 
+CREATE TABLE treatments (
+  id SERIAL PRIMARY KEY,
+  type VARCHAR(25),
+  name VARCHAR(25)
+);
+
 CREATE TABLE invoices (
-id SERIAL PRIMARY KEY,
-total_amount DECIMAL,
-generated_at TIMESTAMP,
-payed_at TIMESTAMP,
-medical_history_id INT PRIMARY KEY,
-FOREIGN KEY (medical_history_id) REFERENCES medical_histories(id)
+  id SERIAL PRIMARY KEY,
+  total_amount DECIMAL,
+  generated_at TIMESTAMP,
+  payed_at TIMESTAMP,
+  medical_history_id INT UNIQUE,
+  FOREIGN KEY (medical_history_id) REFERENCES medical_histories(id)
 );
 
 CREATE TABLE invoice_items (
@@ -27,20 +33,14 @@ unit_price DECIMAL,
 quantity INT,
 total_price DECIMAL,
 invoice_id INT,
-treatment_id INT,
+treatments_id INT,
 FOREIGN KEY (invoice_id) REFERENCES invoices(id),
-FOREIGN KEY (treatment_id) REFERENCES treatments(id)
+FOREIGN KEY (treatments_id) REFERENCES treatments(id)
 );
-
-CREATE TABLE treatments (
-  id SERIAL PRIMARY KEY,
-  type VARCHAR(25),
-  name VARCHAR(25) 
-)
 
 CREATE TABLE medical_histories_treatments (
   medical_histories_id INT,
   treatments_id INT,
   FOREIGN KEY (medical_histories_id) REFERENCES medical_histories(id),
   FOREIGN KEY (treatments_id) REFERENCES treatments(id)
-)
+);
