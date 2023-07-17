5bfa60989872ade16a9438b188370c4a83ed4674
Alejuda <ignacio.fino@mi.unc.edu.ar>
2023-07-17 18:47:01 -0300

    Add tables for treatments and invoice items; add bridge table for the many-to-many relation between medical_historyes and treatments

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -20,3 +20,27 @@ payed_at TIMESTAMP,
 medical_history_id INT,
 FOREIGN KEY (medical_history_id) REFERENCES medical_histories(id)
 );
+
+CREATE TABLE invoice_items (
+id SERIAL PRIMARY KEY,
+unit_price DECIMAL,
+quantity INT,
+total_price DECIMAL,
+invoice_id INT,
+treatment_id INT,
+FOREIGN KEY (invoice_id) REFERENCES invoices(id),
+FOREIGN KEY (treatment_id) REFERENCES treatments(id)
+);
+
+CREATE TABLE treatments (
+  id SERIAL PRIMARY KEY,
+  type VARCHAR(25),
+  name VARCHAR(25) 
+)
+
+CREATE TABLE medical_histories_treatments (
+  medical_histories_id INT,
+  treatments_id INT,
+  FOREIGN KEY (medical_histories_id) REFERENCES medical_histories(id),
+  FOREIGN KEY (treatments_id) REFERENCES treatments(id)
+)
