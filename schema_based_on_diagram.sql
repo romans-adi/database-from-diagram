4c59aa8e68edff376546d839b8cccf7e5852be07
Romans S <romans.spilaks@gmail.com>
2023-07-18 01:50:44 +0300

    Fix the typo

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -24,18 +24,18 @@ CREATE TABLE invoice (
   generated_at TIMESTAMP,
   payed_at TIMESTAMP,
   medical_history_id INT UNIQUE,
-  FOREIGN KEY (medical_history_id) REFERENCES medical_histories(id)
+  FOREIGN KEY (medical_history_id) REFERENCES medical_history(id)
 );
 
 CREATE TABLE invoice_item (
-id SERIAL PRIMARY KEY,
-unit_price DECIMAL,
-quantity INT,
-total_price DECIMAL,
-invoice_id INT,
-treatment_id INT,
-FOREIGN KEY (invoice_id) REFERENCES invoice(id),
-FOREIGN KEY (treatment_id) REFERENCES treatment(id)
+  id SERIAL PRIMARY KEY,
+  unit_price DECIMAL,
+  quantity INT,
+  total_price DECIMAL,
+  invoice_id INT,
+  treatment_id INT,
+  FOREIGN KEY (invoice_id) REFERENCES invoice(id),
+  FOREIGN KEY (treatment_id) REFERENCES treatment(id)
 );
 
 CREATE TABLE medical_history_treatment (
@@ -46,8 +46,13 @@ CREATE TABLE medical_history_treatment (
 );
 
 CREATE INDEX idx_medical_history_patient_id ON medical_history(patient_id);
+
 CREATE INDEX idx_invoice_medical_history_id ON invoice(medical_history_id);
+
 CREATE INDEX idx_invoice_item_invoice_id ON invoice_item(invoice_id);
+
 CREATE INDEX idx_invoice_item_treatment_id ON invoice_item(treatment_id);
+
 CREATE INDEX idx_medical_history_treatment_medical_history_id ON medical_history_treatment(medical_history_id);
+
 CREATE INDEX idx_medical_histories__treatment_id ON medical_history_treatment(treatment_id);
