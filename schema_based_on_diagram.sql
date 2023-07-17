80527b6ae14a937217f27024a981faa4ddc4b04f
Romans S <romans.spilaks@gmail.com>
2023-07-18 01:28:00 +0300

    Use singular table names instead of plural to follow a consistent naming convention

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -1,24 +1,24 @@
-CREATE TABLE patients (
+CREATE TABLE patient (
   id SERIAL PRIMARY KEY,
   name VARCHAR(25),
   date_of_birth DATE
 );
 
-CREATE TABLE medical_histories (
+CREATE TABLE medical_history (
   id SERIAL PRIMARY KEY,
   admitted_at TIMESTAMP,
   patient_id INT,
   status VARCHAR(25),
-  FOREIGN KEY(patient_id) REFERENCES patients(id)
+  FOREIGN KEY(patient_id) REFERENCES patient(id)
 );
 
-CREATE TABLE treatments (
+CREATE TABLE treatment (
   id SERIAL PRIMARY KEY,
   type VARCHAR(25),
   name VARCHAR(25)
 );
 
-CREATE TABLE invoices (
+CREATE TABLE invoice (
   id SERIAL PRIMARY KEY,
   total_amount DECIMAL,
   generated_at TIMESTAMP,
@@ -27,20 +27,20 @@ CREATE TABLE invoices (
   FOREIGN KEY (medical_history_id) REFERENCES medical_histories(id)
 );
 
-CREATE TABLE invoice_items (
+CREATE TABLE invoice_item (
 id SERIAL PRIMARY KEY,
 unit_price DECIMAL,
 quantity INT,
 total_price DECIMAL,
 invoice_id INT,
-treatments_id INT,
-FOREIGN KEY (invoice_id) REFERENCES invoices(id),
-FOREIGN KEY (treatments_id) REFERENCES treatments(id)
+treatment_id INT,
+FOREIGN KEY (invoice_id) REFERENCES invoice(id),
+FOREIGN KEY (treatment_id) REFERENCES treatment(id)
 );
 
-CREATE TABLE medical_histories_treatments (
-  medical_histories_id INT,
-  treatments_id INT,
-  FOREIGN KEY (medical_histories_id) REFERENCES medical_histories(id),
-  FOREIGN KEY (treatments_id) REFERENCES treatments(id)
+CREATE TABLE medical_history_treatment (
+  medical_history_id INT,
+  treatment_id INT,
+  FOREIGN KEY (medical_history_id) REFERENCES medical_history(id),
+  FOREIGN KEY (treatment_id) REFERENCES treatment(id)
 );
